64068030ef2585ceaf3af13713b38c4ce98753aa
arlind00 <aarlind@live.com>
2022-06-29 15:38:30 +0200

    sprint3 1st commit

--- src/test/java/com/sprint/pages/LogIn_page.java
+++ src/test/java/com/sprint/pages/LogIn_page.java
@@ -1,4 +1,29 @@
 package com.sprint.pages;
 
+import com.sprint.utilities.Driver;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
 public class LogIn_page {
+
+    public LogIn_page() {
+        PageFactory.initElements(Driver.getDriver(), this);
+    }
+
+
+    @FindBy(xpath = "//input[@id=\"login\"]")
+    public WebElement userNameInput;
+
+
+    @FindBy(xpath = "//input[@id=\"password\"]")
+    public WebElement passwordInput;
+
+
+    @FindBy(xpath = "//button[.=\"Log in\"]")
+    public WebElement logInButton;
 }
+
+
--- src/test/java/com/sprint/runners/CukesRunner.java
+++ src/test/java/com/sprint/runners/CukesRunner.java
@@ -5,18 +5,19 @@ import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
+
 @RunWith(Cucumber.class)
 @CucumberOptions(
         plugin = {
                 "pretty",
                 "html:target/cucumber-reports.html", // we first right the location where we want to open, then open cucumber-report.html
                 "rerun:target/rerun.txt",            // for failed tests we provide a new page in order those test to be retested
-                "me.jvt.cucumber.report.PrettyReports:target/cucumber"
+                //"me.jvt.cucumber.report.PrettyReports:target/cucumber"
         },
         features = "src/test/resources/features",
         glue = "com/sprint/step_definitions",
         dryRun = false,                               // turns off running step_definitions, just check for missing snippet,  when true= ON, when false = OFF
-        tags = "@smoke",
+        tags = "@wip",
         publish = true                                // generate a link to access reports through an auto generated URL in console
 )
 
